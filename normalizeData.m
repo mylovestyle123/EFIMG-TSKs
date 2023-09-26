522c156e1b257285eb0101ff104a4626e0d350fc
mylovestyle123 <145745425+mylovestyle123@users.noreply.github.com>
2023-09-26 16:06:11 +0800

    Delete normalizeData.m

--- normalizeData.m
+++
@@ -1,8 +0,0 @@
-function [data_out] = normalizeData(data_in)
-data_n = size(data_in,1);
-min_values = min(data_in);
-max_values = max(data_in);
-repmat_min_values = repmat(min_values,data_n,1);
-repmat_max_values = repmat(max_values,data_n,1);
-data_out = (data_in-repmat_min_values)./(repmat_max_values-repmat_min_values);
-end
